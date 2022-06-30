4f2edbb2c942e405048b53ddddde8c8db7624ffd
FazeelRafiq <malikfazeel@gmail.com>
2022-06-30 14:25:21 +0500

    add register api and remove date auto add

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from authentication import views
 
 urlpatterns = [
+    path('api/register/', views.RegisterAPIView.as_view(), name='register'),
     path('api/login/', views.LoginAPIView.as_view(), name='login'),
     path('api/profile/', views.ProfileAPIView.as_view(), name='profile'),
 ]
