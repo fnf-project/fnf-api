666c80cfa335ad8bce265e6c6af7dba56196ca73
FazeelRafiq <malikfazeel@gmail.com>
2022-07-04 20:24:53 +0500

    add superuser login for web

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,5 +5,6 @@ from authentication import views
 urlpatterns = [
     path('api/register/', views.RegisterAPIView.as_view(), name='register'),
     path('api/login/', views.LoginAPIView.as_view(), name='login'),
+    path('api/login-super/', views.LoginSuperUserAPIView.as_view(), name='login_super'),
     path('api/profile/', views.ProfileAPIView.as_view(), name='profile'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.hashers import check_password
-from django.http import Http404
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_200_OK
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
+from rest_framework import exceptions
 
 from authentication.models import User
 from authentication.serializers import UserRegisterSerializer, ProfileSerializer
@@ -40,9 +40,9 @@ class LoginAPIView(APIView):
             if check:
                 return user
             else:
-                raise Http404
+                raise exceptions.AuthenticationFailed('Invalid Password')
         except User.DoesNotExist:
-            raise Http404
+            raise exceptions.AuthenticationFailed('No such user')
 
     def generate_token(self, user):
         try:
@@ -54,6 +54,43 @@ class LoginAPIView(APIView):
     def post(self, request):
         data = request.data
         user = self.validate_user(data['username'], data['password'])
+        serializer = ProfileSerializer(user)
+        data = serializer.data
+
+        try:
+            token = self.generate_token(user)
+            data.update({'token': token.key})
+            return Response(data, status=HTTP_200_OK)
+        except HTTP_400_BAD_REQUEST:
+            return Response("Bad Request", status=HTTP_400_BAD_REQUEST)
+
+class LoginSuperUserAPIView(APIView):
+
+    permission_classes = (AllowAny,)
+
+    def validate_superuser(self, username, password):
+        try:
+            user = User.objects.get(username=username)
+            if not user.is_superuser:
+                raise exceptions.PermissionDenied()
+            check = check_password(password, user.password)
+            if check:
+                return user
+            else:
+                raise exceptions.AuthenticationFailed('Invalid Password')
+        except User.DoesNotExist:
+            raise exceptions.AuthenticationFailed('No such user')
+
+    def generate_token(self, user):
+        try:
+            Token.objects.get(user=user).delete()
+        except:
+            pass
+        return Token.objects.create(user=user)
+
+    def post(self, request):
+        data = request.data
+        user = self.validate_superuser(data['username'], data['password'])
         serializer = ProfileSerializer(user)
         data = serializer.data
 
